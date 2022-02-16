589ac730937e5335cd37bdad36ee84f3b3d909f3
HBK <sammcowen@gmail.com>
2022-02-15 19:48:10 -0800

    questions

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
 
+inquirer
+.prompt([
+  {
+    type:'input',
+    name: 'name',
+    message:'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
+// const fs = require('fs');
+// const { generate } = require('rxjs');
+// const generatePage = require('./src/page-template');
 
-    
-  const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-  };
-  printProfileData(profileDataArgs);
+// const pageHTML = generatePage(name , github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if(err) throw err;
+//   console.lof('porfotlio complete! checkout index.html to see the output!';)
+// });
